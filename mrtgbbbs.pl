8832b39f64b622c7e52e8d214be128418b78383f
R.J. Clay <jame@users.sourceforge.net>
2010-07-13 20:38:06 +0000

    Update mrtgbbbs copyright years to 2010
    git-svn-id: https://ftnpl.svn.sourceforge.net/svnroot/ftnpl/trunk/scripts/BBBS/mrtgbbbs@580 987ebf5b-b7b1-44df-af0d-bd292689ea74

--- mrtgbbbs.pl
+++ mrtgbbbs.pl
@@ -3,7 +3,7 @@
 # mrtgbbbs.pl
 #
 # version 1.2 
-# Copyright (c) 2000-2004 Robert James Clay.  All Rights Reserved.
+# Copyright (c) 2000-2010 Robert James Clay.  All Rights Reserved.
 # This is free software;  you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 
